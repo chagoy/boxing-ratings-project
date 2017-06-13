5af1bf36c17ba493e55cabb6eb6330b3cfc89b8c
chagoy <angelochagoy@gmail.com>
2017-06-13 10:04:13 -0700

    sorted some cosmetic issues out. made things look nice

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ratings</title>
+    <title>Boxing Ratings Database - 0.1</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.1/css/bulma.min.css" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
